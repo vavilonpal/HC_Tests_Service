a55cbdf6cc05b112cb0e552357dc011c3263f11f
Alexeev <sergey50878@mail.ru>
2025-06-03 23:28:46 +0300

    added security and jwt config

--- src/main/java/org/combs/micro/hc_tests_service/configuration/security/SecurityConfig.java
+++ src/main/java/org/combs/micro/hc_tests_service/configuration/security/SecurityConfig.java
@@ -1,10 +1,9 @@
 package org.combs.micro.hc_tests_service.configuration.security;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class WebSecurityConfig {
+public class SecurityConfig {
 
 }
