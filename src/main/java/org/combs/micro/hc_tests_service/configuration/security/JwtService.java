a340d56922ba3541d0e46d9473c527448100a735
Alexeev <sergey50878@mail.ru>
2025-06-03 23:29:12 +0300

    added security and jwt config

---
+++ src/main/java/org/combs/micro/hc_tests_service/configuration/security/JwtService.java
@@ -0,0 +1,29 @@
+package org.combs.micro.hc_tests_service.configuration.security;
+
+import com.nimbusds.jwt.JWTClaimsSet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@RequiredArgsConstructor
+public class JwtService {
+    private final String issuer;
+
+    private final Duration ttl;
+
+    private final JwtEncoder jwtEncoder;
+
+    public String generateToken(final String username){
+        final var claimsSet = JwtClaimsSet.builder()
+                .subject(username)
+                .issuer(issuer)
+                .expiresAt(Instant.now().plus(ttl))
+                .build();
+        return jwtEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
+
+    }
+}
--- src/main/java/org/combs/micro/hc_tests_service/configuration/security/SecurityConfig.java
+++ src/main/java/org/combs/micro/hc_tests_service/configuration/security/SecurityConfig.java
@@ -1,9 +1,50 @@
 package org.combs.micro.hc_tests_service.configuration.security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/v1/auth").permitAll())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(server -> server
+                        .jwt(Customizer.withDefaults())
+                        .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
+                )
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            final AuthenticationConfiguration configuration
+    ) throws Exception {
+        return configuration.getAuthenticationManager();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
